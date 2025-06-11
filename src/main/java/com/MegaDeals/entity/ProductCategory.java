cf9f7a2ad51d7faab361a7655f5e4fc94507db3c
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-11 16:28:06 +0300

    Creating ProductCategory class

---
+++ src/main/java/com/MegaDeals/entity/ProductCategory.java
@@ -0,0 +1,31 @@
+package com.MegaDeals.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class ProductCategory {
+    @EmbeddedId
+    private ProductCategoryID id;
+
+    @ManyToOne
+    @MapsId("categoryID")
+    private Category category;
+
+    @ManyToOne
+    @MapsId("productID")
+    private Product product;
+
+    @Embeddable
+    public class ProductCategoryID {
+        private Long productID;
+        private Long categoryID;
+    }
+}
