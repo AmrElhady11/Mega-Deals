d6103e388b94359faa4f64513c644f87253c4347
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-12 21:02:53 +0300

    creating ErrorResponse

---
+++ src/main/java/com/MegaDeals/response/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.MegaDeals.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private String message;
+    private int code;
+    private Long timestamp;
+}
